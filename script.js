5c9d71c541331d2644d20b6a899b3b8f71854689
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-06-23 01:32:43 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log('javascript added');
+
+let image1Btn = document.querySelector('#image1');
+let image2Btn = document.querySelector('#image2');
+let image3Btn = document.querySelector('#image3');
+let image4Btn = document.querySelector('#image4');
+let bike = document.getElementById('bike');
+
+
+image1Btn.addEventListener('click', () => {
+
+    console.log('image1 is clicked');
+    bike.style.backgroundImage = 'url(images/Converse.webp)';
+
+});
+
+image2Btn.addEventListener('click', () => {
+
+    console.log('image2 is clicked');
+    bike.style.backgroundImage = 'url(images/n2.webp)';
+
+});
+
+image3Btn.addEventListener('click', () => {
+
+    console.log('image3 is clicked');
+    bike.style.backgroundImage = 'url(images/Jordan.webp)';
+
+});
+
+image4Btn.addEventListener('click', () => {
+
+
+    console.log('image4 is clicked');
+    bike.style.backgroundImage = 'url(images/image4.jpg)';
+
+});
+
+
+
+
+
+
